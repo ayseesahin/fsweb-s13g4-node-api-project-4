ff691575e5ca6722f9a68ee538462755439ab397
ayseesahin <aysesah-95@gmail.com>
2023-05-19 02:02:29 +0300

    finished

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-
+const cors = require("cors");
 const server = express();
 const router = require("./users/users-router");
 
 server.use(express.json());
+server.use(cors());
 server.use("/api", router);
 
 server.get("/", (req, res) => {
-  res.json({ message: "is working" });
+  res.json({ message: "express is working" });
 });
 
 module.exports = server;
