fb8fd1b8c5a8a85b49e53e9807b0507bc93db053
ayseesahin <aysesah-95@gmail.com>
2023-05-19 00:59:47 +0300

    finished

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+const router = require("./users/users-router");
+
+server.use(express.json());
+server.use("/api", router);
+
+server.get("/", (req, res) => {
+  res.json({ message: "is working" });
+});
+
+module.exports = server;
---
+++ api/users/users-middleware.js
@@ -0,0 +1,47 @@
+const userModel = require("./users-model");
+
+function validatePayload(req, res, next) {
+  try {
+    let { username, password } = req.body;
+    if (!username || !password) {
+      res.status(404).json({ message: "girilen alanları kontrol ediniz" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+function validateUserNameIsUnique(req, res, next) {
+  try {
+    let { username } = req.body;
+    const isExist = userModel
+      .getAllUsers()
+      .find((item) => item.username === username);
+    if (isExist) {
+      res.status(400).json({ message: "aynı kullanıcı adı mevcut." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function validateLogin(req, res, next) {
+  try {
+    let { username, password } = req.body;
+    const isExist = userModel
+      .getAllUsers()
+      .find((item) => item.username === username && item.password == password);
+    if (!isExist) {
+      res.status(400).json({ message: "giriş bilgilerinizde hata var." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { validatePayload, validateUserNameIsUnique, validateLogin };
